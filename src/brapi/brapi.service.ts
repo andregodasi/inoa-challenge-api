545d3d1b209c14654a68f0e4e7d1a41c51c4ffc0
andregodasi <andre.godasi@gmail.com>
2024-06-16 22:15:59 -0300

    fix: response Brapi

--- src/brapi/brapi.service.ts
+++ src/brapi/brapi.service.ts
@@ -1,36 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { firstValueFrom, map } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class BrapiService {
   constructor(private readonly httpService: HttpService) {}
 
+  private configGetBrapi(url: string) {
+    return this.httpService.get(`${url}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.BRAPI_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
   async findQuoteAll() {
     const resp = await firstValueFrom(
-      this.httpService
-        .get(`${process.env.BRAPI_URL}/quote/list`, {
-          headers: {
-            Authorization: `Bearer ${process.env.BRAPI_TOKEN}`,
-            'Content-Type': 'application/json',
-          },
-        })
-        .pipe(map((httpData) => httpData.data)),
+      this.configGetBrapi(`${process.env.BRAPI_URL}/quote/list`),
     );
-
     return resp.data;
   }
 
   async findQuoteByTicker(ticker: string) {
     const resp = await firstValueFrom(
-      this.httpService
-        .get(`${process.env.BRAPI_URL}/quote/${ticker}?range=3mo&interval=1d`, {
-          headers: {
-            Authorization: `Bearer ${process.env.BRAPI_TOKEN}`,
-            'Content-Type': 'application/json',
-          },
-        })
-        .pipe(map((httpData) => httpData.data)),
+      this.configGetBrapi(
+        `${process.env.BRAPI_URL}/quote/${ticker}?range=3mo&interval=1d`,
+      ),
     );
 
     return resp.data;
