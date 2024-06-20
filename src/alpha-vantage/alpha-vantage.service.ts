c69bc7dfdd89b7ea78e8d776994de543e69f4f19
andregodasi <andre.godasi@gmail.com>
2024-06-20 02:53:08 -0300

    feat: add fallback on the alpha-vantage

--- src/alpha-vantage/alpha-vantage.service.ts
+++ src/alpha-vantage/alpha-vantage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
+import { stocksHistory } from './fallback';
 
 @Injectable()
 export class AlphaVantageService {
@@ -9,9 +10,14 @@ export class AlphaVantageService {
   async findTimeSeriesDailyBySymbol(symbol: string) {
     const resp = await firstValueFrom(
       this.httpService.get(
-        `${process.env.ALPHA_VANTAGE_URL}?function=TIME_SERIES_DAILY&symbol=${symbol}.SA&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`,
+        `${process.env.ALPHA_VANTAGE_URL}?function=TIME_SERIES_MONTHLY&symbol=${symbol}.SA&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`,
       ),
     );
+
+    if (resp.data['Information']) {
+      return stocksHistory;
+    }
+
     return resp.data;
   }
 }
